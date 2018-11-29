7f29664cd90ae07f84f7596329c2cd15c2438df4
800fideos <45268385+800fideos@users.noreply.github.com>
2018-11-29 20:11:25 +0100

    2
    segundo ejercicio a medias

---
+++ EjerciciosJavi/Assets/Scripts/andarRaton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class andarRaton : MonoBehaviour {
+
+    Vector2 posicion_objetivo;
+    public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 posicionMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 nueva_posicion = new Vector2 (posicionMouse.x, transform.position.y); //la posicion en x va a ser la del raton y la y la mia (donde deje el personaje)
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.//cada vez que haga click me voy a la posicion objetivo
+        }
+	}
+}
---
+++ EjerciciosJavi/Assets/Scripts/andarTeclado.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class andarTeclado : MonoBehaviour {
+
+    public float velocidad = 30;
+    private Rigidbody2D rg;
+
+	// Use this for initialization
+	void Start () {
+        rg = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float velocidad_y = rg.velocity.y;
+        float velocidad_x = Input.GetAxis("Horizontal") * velocidad;
+
+        Vector2 vectorVelocidad = new Vector2(velocidad_x, velocidad_y); //vector que une ambos ejes
+
+        rg.velocity = vectorVelocidad;
+    }
+}
